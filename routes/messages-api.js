6ec557a0d60b2f91bf620da27ac90a982e142efe
Edlyn Shih <edlyn0418@gmail.com>
2023-03-12 23:47:06 -0400

    update query to get private messages between two users

--- routes/messages-api.js
+++ routes/messages-api.js
@@ -17,17 +17,28 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.get('/', (req, res) => {
+router.get('/chat/:id/:otherId', (req, res) => {
+  console.log('HELLLLLLLO')
+  console.log('HERE IS REQ.PARAMS >>>>>>>>>>>', req.params)
   return db.query(`
-  SELECT *
+  SELECT messages.*, fp.name as from_pet_name, tp.name as to_pet_name, fp.photo_url as from_pet_photo_url, tp.photo_url as to_pet_photo_url
   FROM messages
-  `)
+  JOIN pets fp ON fp.id = messages.from_petId
+  JOIN pets tp ON tp.id = messages.to_petId
+  WHERE messages.timestamp IN (
+  SELECT MAX(timestamp)
+  FROM messages
+  WHERE (from_petId = $1 AND to_petId = $2) OR (from_petId = $2 AND to_petId = $1)
+  GROUP BY CASE
+    WHEN from_petId = $1 AND to_petId = $2 THEN CONCAT(from_petId, to_petId)
+    WHEN from_petId = $2 AND to_petId = $1 THEN CONCAT(from_petId, to_petId)
+  END
+  )
+  ORDER BY messages.timestamp ASC;
+  `, [Number(req.params.otherId), Number(req.params.id)])
     .then(({ rows: messages }) => {
       res.json(
-        messages.reduce(
-          (previous, current) => ({ ...previous, [current.id]: current }),
-          {}
-        )
+       messages
       );
     });
 });
